83a8d2424c23b703952c6e73896f4646dc064a99
HarisAja68 <samsudinharis3@gmail.com>
2021-08-31 21:55:27 +0700

    tambah jetstream

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -28,6 +28,8 @@ class CreateUsersTable extends Migration
             $table->string('path_image')->nullable();
             $table->unsignedBigInteger('role_id');
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
+            $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
     }
